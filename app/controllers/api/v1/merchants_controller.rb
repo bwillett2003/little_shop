4ecbab495492711bf5f75d61d84dfe53f888c9ed
John Hill <jphill1989@gmail.com>
2024-09-11 17:38:56 -0600

    Added test and method for create

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -3,4 +3,15 @@ class Api::V1::MerchantsController < ApplicationController
     merchants = Merchant.all
     render json: MerchantsSerializer.new(merchants)
   end
+
+  def create
+    merchant = Merchant.create(merchant_params)
+    render json: MerchantsSerializer.new(merchant)
+  end
+
+  private
+
+  def merchant_params
+    params.require(:merchant).permit(:name)
+  end
 end
--- spec/requests/api/v1/merchant_request_spec.rb
+++ spec/requests/api/v1/merchant_request_spec.rb
@@ -1,28 +1,63 @@
 require 'rails_helper'
 
 RSpec.describe "Merchants" do
-  it "gets a list of all merchants" do
-    Merchant.create!(name: "Walmart")
-    Merchant.create!(name: "Target")
-    Merchant.create!(name: "Sam's")
 
-    get "/api/v1/merchants"
+  describe "index" do
+    it "gets a list of all merchants" do
+      Merchant.create!(name: "Walmart")
+      Merchant.create!(name: "Target")
+      Merchant.create!(name: "Sam's")
 
-    expect(response).to be_successful
+      get "/api/v1/merchants"
 
-    merchants = JSON.parse(response.body, symbolize_names: true)
-    expect(merchants[:data].count).to eq(3)
+      expect(response).to be_successful
 
-    merchants[:data].each do |merchant|
+      merchants = JSON.parse(response.body, symbolize_names: true)
+      expect(merchants[:data].count).to eq(3)
+
+      merchants[:data].each do |merchant|
+
+        expect(merchant).to have_key(:id)
+        expect(merchant[:id]).to be_an(String)
+
+        expect(merchant).to have_key(:attributes)
+        attributes = merchant[:attributes]
+
+        expect(attributes).to have_key(:name)
+        expect(attributes[:name]).to be_a(String)
+      end
+    end
+  end
+
+  describe "create" do
+    it "can create a resource" do
+      merchant_params = { name: "Costco"}
+
+
+
+      headers = { "CONTENT_TYPE" => "application/json" }
+      post "/api/v1/merchants", headers: headers, params: JSON.generate(merchant_params)
+
+      expect(response).to be_successful
+
+      created_merchant = Merchant.last
+      expect(created_merchant).to eq(merchant_params[:name])
+
+      merchants_data = JSON.parse(response.body, symbolize_names: true)
+      merchant = merchants_data[:data]
 
       expect(merchant).to have_key(:id)
       expect(merchant[:id]).to be_an(String)
+      expect(merchant[:id]). to eq(created_merchant.id)
+  
+      expect(merchant).to have_key(:type)
+      expect(merchant[:type]).to be_a(String)
 
-      expect(merchant).to have_key(:attributes)
       attributes = merchant[:attributes]
 
       expect(attributes).to have_key(:name)
       expect(attributes[:name]).to be_a(String)
+      expect(attributes[:name]).to eq(created_merchant.name)
     end
   end
 end
