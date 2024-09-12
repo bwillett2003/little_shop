bf5ec4ca494905fede06bba6a23b99253cc31ecb
James-Cochran <jameslikesfruit@gmail.com>
2024-09-12 14:06:33 -0400

    added validation statements to all of the models

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,4 +1,10 @@
 class InvoiceItem < ApplicationRecord
   belongs_to :item
   belongs_to :invoice
+
+  validates :item_id, presence: true, numericality: { only_integer: true}
+  validates :invoice_id, presence: true, numericality: {only_integer: true}
+  validates :quantity, presence: true, numericality: {only_integer: true}
+  validates :unit_price, presence: true, numericality: {only_float: true}
+  
 end
