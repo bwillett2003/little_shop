7853be6134679eb005d057bd96a8a973af861634
James-Cochran <jameslikesfruit@gmail.com>
2024-09-12 18:14:18 -0400

    updated merchant items spec to include data type validation.

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -19,4 +19,27 @@ class Api::V1::MerchantsController < ApplicationController
       }, status: 404
     end
   end
+
+  def update
+    begin
+      updated_merchant = Merchant.find(params[:id])
+      render json: MerchantSerializer.new(updated_merchant)
+    rescue ActiveRecord::RecordNotFound
+      render json: {
+        errors: [
+          {
+            status: "404", 
+            message: "Record not found."
+          }
+        ]
+      }, status: 404
+    end
+  end
+  
+
+  private
+
+  def merchant_params
+    params.require(:merchant).permit(:name)
+  end
 end
--- spec/requests/api/v1/merchant_items_request_spec.rb
+++ spec/requests/api/v1/merchant_items_request_spec.rb
@@ -20,11 +20,21 @@ RSpec.describe "Merchant Items" do
         expect(items[:data].count).to eq(2)
 
         items[:data].each do |item|
+
           expect(item).to have_key(:id)
+          expect(item[:id]).to be_a(String)
+
           expect(item[:attributes]).to have_key(:name)
+          expect(item[:attributes][:name]).to be_a(String)
+
           expect(item[:attributes]).to have_key(:description)
+          expect(item[:attributes][:description]).to be_a(String)
+
           expect(item[:attributes]).to have_key(:unit_price)
+          expect(item[:attributes][:unit_price]).to be_a(Float)
+
           expect(item[:attributes]).to have_key(:merchant_id)
+          expect(item[:attributes][:merchant_id]).to be_a(Integer)
           expect(item[:attributes][:merchant_id]).to eq(walmart.id)
         end
     end
