a3a54a0d1ce811dfef413ee0e118b12714ad5712
Shawn Thompson <thompson_shawn@hotmail.com>
2024-09-14 11:07:04 -0500

    test: added model tests for asc and desc order

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -11,5 +11,26 @@ RSpec.describe Merchant do
     it {should validate_uniqueness_of(:name)}
   end
 
-  
+  describe 'sorting methods' do
+    it "gets a list of all merchants in ascending order" do
+      merchant_1 = Merchant.create!(name: "Walmart")
+      merchant_2 = Merchant.create!(name: "Target")
+      merchant_3 = Merchant.create!(name: "Sam's")
+
+      sorted_merchants = Merchant.sort_direction('asc')
+          
+      expect(sorted_merchants).to eq([merchant_1, merchant_2, merchant_3])
+    end
+
+    it "gets a list of all merchants in descending order" do
+      merchant_1 = Merchant.create!(name: "Walmart")
+      merchant_2 = Merchant.create!(name: "Target")
+      merchant_3 = Merchant.create!(name: "Sam's")
+
+      sorted_merchants = Merchant.sort_direction('desc')
+          
+      expect(sorted_merchants).to eq([merchant_3, merchant_2, merchant_1])
+    end
+  end
+
 end
