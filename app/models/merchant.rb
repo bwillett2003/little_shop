47bc265040616c2357ab25bd669e48f0dc2b8be5
Shawn Thompson <thompson_shawn@hotmail.com>
2024-09-15 13:52:01 -0500

    Update code to be uniform

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,12 +3,12 @@ class Merchant < ApplicationRecord
   has_many :items, dependent: :destroy
 
   def self.sort_direction(sort)   
-    return order(created_at: :desc) if sort == 'age'
+    return order(created_at: :desc) if sort == "age"
     return all
   end
 
   def self.filter_returned(status)
-    return joins(:invoices).where(invoices: {status: 'returned'}).distinct if status == 'returned'
+    return joins(:invoices).where(invoices: {status: "returned"}).distinct if status == "returned"
     return all
  
   end
